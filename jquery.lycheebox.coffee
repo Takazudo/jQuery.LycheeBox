3a5d48ecb473c7f1a7d108c544251dcd1f4d1205
Takeshi Takatsudo <takazudo@gmail.com>
2013-10-01 21:48:07 +0900

    implemented base things

--- jquery.lycheebox.coffee
+++ jquery.lycheebox.coffee
@@ -1,27 +1,204 @@
 do ($ = jQuery) ->
 
+  $window = $(window)
+  $document = $(document)
+  domwindowApi = null
+
   # define namespaces
+
   ns = {}
 
-  # misc
-  
-  wait = (time) ->
-    $.Deferred (defer) ->
-      setTimeout ->
-        defer.resolve()
-      , time
+  # ============================================================
+  # initializer
+
+  ns.setup = do ->
+    setupDone = false
+    return ->
+      return if setupDone
+      setupDone = true
+      $.ui.domwindowdialog.setup
+        width: 600
+        height: 300
+      domwindowApi = window.domwindowApi
 
   # ============================================================
   # util
-  
+
+  ns.viewport =
+    width: -> $window.width()
+    height: -> window.innerHeight or $window.height()
+    createSizeObject: ->
+      o =
+        width: ns.viewport.width()
+        height: ns.viewport.height()
+      return o
 
   # ============================================================
   # Dialog
   
   class ns.Dialog
 
-    constructor: ->
-      console.log 'lychee!'
+    @defaults =
+      dialog_src: """
+        <div class="lycheebox-dialog">
+          <div class="lycheebox-positioner">
+            <div class="lycheebox-main">
+              <div class="lycheebox-main-inner">
+                <div class="lycheebox-imgholder"></div>
+                <a class="lycheebox-dialogcloser apply-domwindow-close" href="#">close</a>
+              </div>
+            </div>
+          </div>
+          <div class="lycheebox-loader"></div>
+        </div>
+      """
+      spinner_options:
+        color: '#fff'
+        lines: 20
+        length: 10
+        width: 2
+        radius: 40
+      selector_dialogRoot: '.lycheebox-dialog'
+      selector_loader: '.lycheebox-loader'
+      selector_holder: '.lycheebox-imgholder'
+      selector_main: '.lycheebox-main'
+      dialog_LR_margin: 10
+      dialog_TB_margin: 10
+      dialog_LR_padding: 10
+      dialog_TB_padding: 10
+      dialog_closer_height: 35
+
+    
+    constructor: (@$opener, options) ->
+
+      @options = $.extend {}, ns.Dialog.defaults, options
+      @_eventify()
+
+    open: ->
+
+      ns.setup()
+      dialog_src = $.trim @options.dialog_src
+
+      openOptions = $.extend
+        strdialog: true
+        tandbmargintodecideposition: 0
+        afteropen: (e, data) =>
+          @$dialog = data.dialog
+          @_handleAfterOpen()
+        beforeclose: =>
+          @$dialog = null
+          @_handleBeforeClose()
+      , ns.viewport.createSizeObject()
+
+      domwindowApi.open @options.dialog_src, openOptions
+
+    destroy: ->
+
+      @$opener.unbind 'click', @_openerClickHandler
+      $window.unbind 'resize orientationchange', @_resizeHandler
+
+    resizeDialog: ->
+
+      return unless @opened
+      size =  ns.viewport.createSizeObject()
+      @$dialog.css size
+
+    resizeEls: ->
+      
+      return unless @opened
+      o = @options
+
+      imgSize = $.imgUtil.calcRectContainImgWH
+        imgWidth: @naturalImgSize.width
+        imgHeight: @naturalImgSize.height
+        rectWidth: ns.viewport.width() - (o.dialog_LR_margin * 2) - (o.dialog_LR_padding * 2)
+        rectHeight: ns.viewport.height() - (o.dialog_TB_margin * 2) - (o.dialog_TB_padding * 2) - o.dialog_closer_height
+        enlargeSmallImg: false
+
+      w = imgSize.width + (o.dialog_LR_padding * 2)
+      h = imgSize.height + (o.dialog_TB_padding * 2) + o.dialog_closer_height
+      mainSize =
+        width: w
+        height: h
+        marginTop: -1 * h / 2
+        marginLeft: -1 * w / 2
+
+      @$main.css mainSize
+      @$holder.css imgSize
+
+
+    _handleAfterOpen: ->
+
+      @opened = true
+      @imgsrc = @$opener.attr 'href'
+      @_eventify_overlayClickClose()
+      @_putSpinner()
+      @_calcImgSize().then =>
+        return @_removeSpinner()
+      .then =>
+        @_prepareElsInsideDialog()
+        @resizeEls()
+        @$holder.append @$img
+        @$main.fadeIn()
+
+    _handleBeforeClose: ->
+
+      @opened = false
+      @_uneventify_overlayClickClose()
+
+    _eventify_overlayClickClose: ->
+
+      o = @options
+      @_overlayClickHandler = (e) =>
+        $target = $(e.target)
+        return unless $target.is "#{o.selector_loader}, #{o.selector_dialogRoot}"
+        domwindowApi.close()
+      $document.delegate o.selector_dialogRoot, 'click', @_overlayClickHandler
+
+    _uneventify_overlayClickClose: ->
+
+      o = @options
+      $document.undelegate o.selector_dialogRoot, 'click', @_overlayClickHandler
+
+    _putSpinner: ->
+
+      o = @options
+      @$loader = $(o.selector_loader, @$dialog)
+      (new Spinner o.spinner_options).spin @$loader[0]
+
+    _calcImgSize: ->
+
+      defer = $.Deferred()
+      ($.imgUtil.calcNaturalWH @imgsrc).done (wh, $img) =>
+        @naturalImgSize = wh
+        @$img = $img
+        defer.resolve()
+      return defer.promise()
+
+    _removeSpinner: ->
+
+      defer = $.Deferred()
+      ($.when @$loader.fadeOut()).done =>
+        @$loader.empty().remove()
+        defer.resolve()
+      return defer.promise()
+
+    _eventify: ->
+
+      @_openerClickHandler = (e) =>
+        e.preventDefault()
+        @open()
+      @_resizeHandler = (e) =>
+        @resizeDialog()
+        @resizeEls()
+
+      @$opener.bind 'click', @_openerClickHandler
+      $window.bind 'resize orientationchange', @_resizeHandler
+
+    _prepareElsInsideDialog: ->
+
+      @$main = $(@options.selector_main, @$dialog)
+      @$holder = $(@options.selector_holder, @$dialog)
 
   # ============================================================
   # jQuery bridges
@@ -30,16 +207,13 @@ do ($ = jQuery) ->
     return @each (i, el) ->
       $opener = $(el)
       instance = $opener.data 'lycheebox'
-      if instance
-        instance.open()
-        return
+      if instance?
+        instance.destroy()
       instance = new ns.Dialog $opener, options
       $opener.data 'liycheebox', instance
-      instance.open()
 
   # ============================================================
   # globalify
 
-  $.LycheeBoxNs = ns
-  $.LycheeBox = ns.Dialog
+  $.LycheeBox = ns
 
--- jquery.lycheebox.js
+++ jquery.lycheebox.js
@@ -5,19 +5,218 @@
  * License: MIT */
 (function() {
   (function($) {
-    var ns, wait;
+    var $document, $window, domwindowApi, ns;
+    $window = $(window);
+    $document = $(document);
+    domwindowApi = null;
     ns = {};
-    wait = function(time) {
-      return $.Deferred(function(defer) {
-        return setTimeout(function() {
-          return defer.resolve();
-        }, time);
-      });
+    ns.setup = (function() {
+      var setupDone;
+      setupDone = false;
+      return function() {
+        if (setupDone) {
+          return;
+        }
+        setupDone = true;
+        $.ui.domwindowdialog.setup({
+          width: 600,
+          height: 300
+        });
+        return domwindowApi = window.domwindowApi;
+      };
+    })();
+    ns.viewport = {
+      width: function() {
+        return $window.width();
+      },
+      height: function() {
+        return window.innerHeight || $window.height();
+      },
+      createSizeObject: function() {
+        var o;
+        o = {
+          width: ns.viewport.width(),
+          height: ns.viewport.height()
+        };
+        return o;
+      }
     };
     ns.Dialog = (function() {
-      function Dialog() {
-        console.log('lychee!');
+      Dialog.defaults = {
+        dialog_src: "<div class=\"lycheebox-dialog\">\n  <div class=\"lycheebox-positioner\">\n    <div class=\"lycheebox-main\">\n      <div class=\"lycheebox-main-inner\">\n        <div class=\"lycheebox-imgholder\"></div>\n        <a class=\"lycheebox-dialogcloser apply-domwindow-close\" href=\"#\">close</a>\n      </div>\n    </div>\n  </div>\n  <div class=\"lycheebox-loader\"></div>\n</div>",
+        spinner_options: {
+          color: '#fff',
+          lines: 20,
+          length: 10,
+          width: 2,
+          radius: 40
+        },
+        selector_dialogRoot: '.lycheebox-dialog',
+        selector_loader: '.lycheebox-loader',
+        selector_holder: '.lycheebox-imgholder',
+        selector_main: '.lycheebox-main',
+        dialog_LR_margin: 10,
+        dialog_TB_margin: 10,
+        dialog_LR_padding: 10,
+        dialog_TB_padding: 10,
+        dialog_closer_height: 35
+      };
+
+      function Dialog($opener, options) {
+        this.$opener = $opener;
+        this.options = $.extend({}, ns.Dialog.defaults, options);
+        this._eventify();
       }
+
+      Dialog.prototype.open = function() {
+        var dialog_src, openOptions,
+          _this = this;
+        ns.setup();
+        dialog_src = $.trim(this.options.dialog_src);
+        openOptions = $.extend({
+          strdialog: true,
+          tandbmargintodecideposition: 0,
+          afteropen: function(e, data) {
+            _this.$dialog = data.dialog;
+            return _this._handleAfterOpen();
+          },
+          beforeclose: function() {
+            _this.$dialog = null;
+            return _this._handleBeforeClose();
+          }
+        }, ns.viewport.createSizeObject());
+        return domwindowApi.open(this.options.dialog_src, openOptions);
+      };
+
+      Dialog.prototype.destroy = function() {
+        this.$opener.unbind('click', this._openerClickHandler);
+        return $window.unbind('resize orientationchange', this._resizeHandler);
+      };
+
+      Dialog.prototype.resizeDialog = function() {
+        var size;
+        if (!this.opened) {
+          return;
+        }
+        size = ns.viewport.createSizeObject();
+        return this.$dialog.css(size);
+      };
+
+      Dialog.prototype.resizeEls = function() {
+        var h, imgSize, mainSize, o, w;
+        if (!this.opened) {
+          return;
+        }
+        o = this.options;
+        imgSize = $.imgUtil.calcRectContainImgWH({
+          imgWidth: this.naturalImgSize.width,
+          imgHeight: this.naturalImgSize.height,
+          rectWidth: ns.viewport.width() - (o.dialog_LR_margin * 2) - (o.dialog_LR_padding * 2),
+          rectHeight: ns.viewport.height() - (o.dialog_TB_margin * 2) - (o.dialog_TB_padding * 2) - o.dialog_closer_height,
+          enlargeSmallImg: false
+        });
+        w = imgSize.width + (o.dialog_LR_padding * 2);
+        h = imgSize.height + (o.dialog_TB_padding * 2) + o.dialog_closer_height;
+        mainSize = {
+          width: w,
+          height: h,
+          marginTop: -1 * h / 2,
+          marginLeft: -1 * w / 2
+        };
+        this.$main.css(mainSize);
+        return this.$holder.css(imgSize);
+      };
+
+      Dialog.prototype._handleAfterOpen = function() {
+        var _this = this;
+        this.opened = true;
+        this.imgsrc = this.$opener.attr('href');
+        this._eventify_overlayClickClose();
+        this._putSpinner();
+        return this._calcImgSize().then(function() {
+          return _this._removeSpinner();
+        }).then(function() {
+          _this._prepareElsInsideDialog();
+          _this.resizeEls();
+          _this.$holder.append(_this.$img);
+          return _this.$main.fadeIn();
+        });
+      };
+
+      Dialog.prototype._handleBeforeClose = function() {
+        this.opened = false;
+        return this._uneventify_overlayClickClose();
+      };
+
+      Dialog.prototype._eventify_overlayClickClose = function() {
+        var o,
+          _this = this;
+        o = this.options;
+        this._overlayClickHandler = function(e) {
+          var $target;
+          $target = $(e.target);
+          if (!$target.is("" + o.selector_loader + ", " + o.selector_dialogRoot)) {
+            return;
+          }
+          return domwindowApi.close();
+        };
+        return $document.delegate(o.selector_dialogRoot, 'click', this._overlayClickHandler);
+      };
+
+      Dialog.prototype._uneventify_overlayClickClose = function() {
+        var o;
+        o = this.options;
+        return $document.undelegate(o.selector_dialogRoot, 'click', this._overlayClickHandler);
+      };
+
+      Dialog.prototype._putSpinner = function() {
+        var o;
+        o = this.options;
+        this.$loader = $(o.selector_loader, this.$dialog);
+        return (new Spinner(o.spinner_options)).spin(this.$loader[0]);
+      };
+
+      Dialog.prototype._calcImgSize = function() {
+        var defer,
+          _this = this;
+        defer = $.Deferred();
+        ($.imgUtil.calcNaturalWH(this.imgsrc)).done(function(wh, $img) {
+          _this.naturalImgSize = wh;
+          _this.$img = $img;
+          return defer.resolve();
+        });
+        return defer.promise();
+      };
+
+      Dialog.prototype._removeSpinner = function() {
+        var defer,
+          _this = this;
+        defer = $.Deferred();
+        ($.when(this.$loader.fadeOut())).done(function() {
+          _this.$loader.empty().remove();
+          return defer.resolve();
+        });
+        return defer.promise();
+      };
+
+      Dialog.prototype._eventify = function() {
+        var _this = this;
+        this._openerClickHandler = function(e) {
+          e.preventDefault();
+          return _this.open();
+        };
+        this._resizeHandler = function(e) {
+          _this.resizeDialog();
+          return _this.resizeEls();
+        };
+        this.$opener.bind('click', this._openerClickHandler);
+        return $window.bind('resize orientationchange', this._resizeHandler);
+      };
+
+      Dialog.prototype._prepareElsInsideDialog = function() {
+        this.$main = $(this.options.selector_main, this.$dialog);
+        return this.$holder = $(this.options.selector_holder, this.$dialog);
+      };
 
       return Dialog;
 
@@ -27,17 +226,14 @@
         var $opener, instance;
         $opener = $(el);
         instance = $opener.data('lycheebox');
-        if (instance) {
-          instance.open();
-          return;
+        if (instance != null) {
+          instance.destroy();
         }
         instance = new ns.Dialog($opener, options);
-        $opener.data('liycheebox', instance);
-        return instance.open();
+        return $opener.data('liycheebox', instance);
       });
     };
-    $.LycheeBoxNs = ns;
-    return $.LycheeBox = ns.Dialog;
+    return $.LycheeBox = ns;
   })(jQuery);
 
 }).call(this);
